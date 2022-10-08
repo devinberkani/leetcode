89baf66a56ac358ff3869f3f761667131fd55740
Devin Berkani <devinberkani@gmail.com>
2022-10-08 19:11:02 -0400

    complete remove element leetcode problem

--- easy/RemoveDuplicatesFromSortedArray/src/Main.java
+++ easy/RemoveDuplicatesFromSortedArray/src/Main.java
@@ -1,36 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[] {1,1,2}));
+        System.out.println(removeDuplicates(new int[] {1,2,3}));
     }
 
     public static int removeDuplicates(int[] nums) {
 
-        if (nums.length == 1) {
-            return 1;
-        }
-
         int index = 1;
-        int count = 0;
-        int current = Integer.MAX_VALUE;
-        int next = Integer.MIN_VALUE;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (current != next) {
-                count++;
-            }
-            current = nums[i];
-            if (i != nums.length - 1) {
-                next = nums[i + 1];
-            }
-
-            if (current != next) {
-                nums[index] = next;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+                nums[index] = nums[i];
                 index++;
             }
         }
-        for (int n : nums) {
-            System.out.println(n);
-        }
-        return count;
+        return index;
     }
 }
