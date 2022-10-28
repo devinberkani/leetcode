f7f1c9346c24d1f8641573a8e6f46ddbe09a66b9
Devin Berkani <devinberkani@gmail.com>
2022-10-28 19:52:19 -0400

    complete reverse bits leetcode problem

---
+++ easy/ReverseBits/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(reverseBits(43261596));
+    }
+
+    public static int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+
+            result <<= 1;
+
+            if ((n & 1) == 1) {
+                result++;
+            }
+            n >>= 1;
+
+        }
+
+        return result;
+    }
+}
