af7b3ee3187590fb5306adfb8fe72087c2522f15
Devin Berkani <devinberkani@gmail.com>
2022-11-09 18:24:51 -0500

    complete move zeroes leetcode problem

---
+++ easy/MoveZeroes/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        moveZeroes(new int[] {0});
+    }
+
+    public static void moveZeroes(int[] nums) {
+
+//        for (int n : nums) {
+//            System.out.println(n);
+//        }
+
+        if (nums.length == 1) {
+            return;
+        }
+
+        int nextIndex = 0;
+        int zeroCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nextIndex] = nums[i];
+                nextIndex++;
+            } else {
+                zeroCount++;
+            }
+        }
+
+        for (int i = nums.length - 1; i >= nums.length - zeroCount; i--) {
+            nums[i] = 0;
+        }
+
+        for (int n : nums) {
+            System.out.println(n);
+        }
+
+    }
+}
