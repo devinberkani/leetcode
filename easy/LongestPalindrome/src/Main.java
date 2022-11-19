86760107c6e425f91e0bfd8c0ca73fc5e2eff1c3
Devin Berkani <devinberkani@gmail.com>
2022-11-19 14:25:55 -0500

    complete longest palindrome leetcode problem

---
+++ easy/LongestPalindrome/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("aaaabb"));
+    }
+
+    public static int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c: s.toCharArray())
+            count[c]++;
+
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            if (ans % 2 == 0 && v % 2 == 1)
+                ans++;
+        }
+        return ans;
+    }
+}
