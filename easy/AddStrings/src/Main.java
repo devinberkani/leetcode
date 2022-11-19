a0604d90794a6f0beadfbcac3989325380d27c74
Devin Berkani <devinberkani@gmail.com>
2022-11-19 16:26:42 -0500

    save better result for add strings leetcode problem found in discussions

--- easy/AddStrings/src/Main.java
+++ easy/AddStrings/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(addStrings("1", "2999"));
+        System.out.println(addStrings2("1", "2999"));
     }
 
     public static String addStrings(String num1, String num2) {
@@ -35,5 +35,31 @@ public class Main {
         }
 
         return result.toString();
+    }
+
+    // better solution found in discussions
+
+    public static String addStrings2(String num1, String num2) {
+        int carry=0;
+        StringBuilder result = new StringBuilder();
+        int i=num1.length() -1;
+        int j=num2.length() -1;
+        while(i>=0 || j>=0)
+        {
+
+            int n1 = (i<0)?0:num1.charAt(i)-'0';
+            int n2 = (j<0)?0:num2.charAt(j)-'0';
+
+            int sum=n1+n2+carry;
+            result.append(sum%10);
+            carry=sum/10;
+
+            i--;
+            j--;
+        }
+        if(carry==1){
+            result.append(carry);
+        }
+        return result.reverse().toString();
     }
 }
