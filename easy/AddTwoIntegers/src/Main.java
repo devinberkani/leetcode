851e801526780e5b491f0a76808993a7d81b1909
Devin Berkani <devinberkani@gmail.com>
2022-11-26 22:54:20 -0500

    complete add tw o integers leetcode problem

---
+++ easy/AddTwoIntegers/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sum(-10,4));
+    }
+
+    public static int sum(int num1, int num2) {
+        return Integer.sum(num1, num2);
+    }
+}
