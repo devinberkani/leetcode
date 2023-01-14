4628afc909a38c539ef729931e041154889de8df
Devin Berkani <devinberkani@gmail.com>
2023-01-14 10:13:31 -0500

    change folder structure in leetcode repo to allow for more leetcode language practice

--- python/easy/two-sum.py
+++ python/easy/two-sum.py
@@ -1,19 +1,15 @@
 class Solution(object):
-  def twoSum(self, nums, target):
-          """
-          :type nums: List[int]
-          :type target: int
-          :rtype: List[int]
-          """
-          i = 0
-          while i < len(nums):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        while i < len(nums):
             j = 1
             while j < len(nums):
-              if nums[i] + nums[j] == target:
-                return [i, j]
-            j+=1
-          i += 1
-
-test = Solution()
-
-print(test.twoSum([2,7,11,15], 9))
+                if i != j and nums[i] + nums[j] == target:
+                    return [i, j]
+                j+=1
+            i+=1
