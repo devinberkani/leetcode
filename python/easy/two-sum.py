64118a85f346c2154443e190231c77421a6fdfd2
Devin Berkani <devinberkani@gmail.com>
2023-01-14 10:47:47 -0500

    improve speed of python two sum solution

--- python/easy/two-sum.py
+++ python/easy/two-sum.py
@@ -1,15 +1,30 @@
 class Solution(object):
+    # def twoSum(self, nums, target):
+    #     """
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: List[int]
+    #     """
+    #     i = 0
+    #     while i < len(nums):
+    #         j = 1
+    #         while j < len(nums):
+    #             if i != j and nums[i] + nums[j] == target:
+    #                 return [i, j]
+    #             j+=1
+    #         i+=1
+
+# much faster solution - python hashmap
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        i = 0
-        while i < len(nums):
-            j = 1
-            while j < len(nums):
-                if i != j and nums[i] + nums[j] == target:
-                    return [i, j]
-                j+=1
-            i+=1
+        dict = {}
+        for i in range((len(nums))):
+            dict[nums[i]] = i
+
+        for i in range(len(nums)):
+            if (target - nums[i]) in dict and i != dict[target - nums[i]]:
+                return [i, dict[target - nums[i]]]
+
+
+print(Solution().twoSum([2,7,11,15], 9))
+print(Solution().twoSum([3,2,4], 6))
+print(Solution().twoSum([3,3], 6))
